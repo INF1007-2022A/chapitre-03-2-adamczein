b9d98c6b332f7846d322d8b7e8d430c98d8500ad
adamczein <112838729+adamczein@users.noreply.github.com>
2022-10-16 19:58:10 -0400

    version final

--- exercice.py
+++ exercice.py
@@ -2,50 +2,78 @@
 
 
 def dissipated_power(voltage, resistance):
-	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+    puissance = (voltage ** 2) / resistance
+    return puissance
+
 
 def orthogonal(v1, v2):
-	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+    # TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
+    v1[0]  # Pour accéder au X
+    v1[1]  # Pour accéder au Y
+
+    p_orthogonal = v1[0] * v2[0] + v1[1] * v2[1]
+    if p_orthogonal == 0:
+        return True
+    if not p_orthogonal == 0:
+        return False
+    pass
+
 
 def average(values):
-	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
-	for v in values:
-		pass # La variable v contient une valeur de la liste.
+    # TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+    sum = num_elems = 0
+    for v in values:
+        if v >= 0:
+            sum += v
+            num_elems += 1
+
+    average_sum = sum / num_elems
+    return average_sum
+
+    pass  # La variable v contient une valeur de la liste.
+
 
 def bills(value):
-	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
-	while value != 0:
-		if value >= 20:
-			pass
-		elif value >= 10:
-			pass
-		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
+    # TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+    twenties = tens = fives = ones = 0
+    while value != 0:
+        if value >= 20:
+            twenties += value // 20
+            value = value % 20
 
-	return (twenties, tens, fives, twos, ones);
+        elif value >= 10:
+            tens += value // 10
+            value = value % 10
 
-def format_base(value, base, digit_letters):
-	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
-	# `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
-	result = ""
-	abs_value = abs(value)
-	while abs_value != 0:
-		pass
-	if value < 0:
-		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-		pass
-	return result
+        elif value >= 5:
+            fives += value // 5
+            value = value % 5
+
+        elif value >= 1:
+            ones += value
+            value = 0
+
+    return (twenties, tens, fives, ones);
+
+
+def format_base(value, base, letters):
+    # Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
+    # `digits_letters[0]` Nous donne la lettre pour le chiffre 0, ainsi de suite.
+    result = ""
+    abs_value = abs(value)
+    while abs_value != 0:
+        digit_value = abs_value % base
+        result += letters[digit_value]
+        abs_value //= base
+    if value < 0:
+        result += "-"
+
+    return result[::-1]
 
 
 if __name__ == "__main__":
-	print(dissipated_power(69, 420))
-	print(orthogonal((1, 1), (-1, 1)))
-	print(average([1, 4, -2, 10]))
-	print(bills(137))
-	print(format_base(-42, 16, "0123456789ABCDEF"))
+    print(dissipated_power(69, 420))
+    print(orthogonal((1, 1), (-1, 1)))
+    print(average([1, 4, -2, 10]))
+    print(bills(137))
+    print(format_base(-42, 16, "0123456789ABCDEF"))
